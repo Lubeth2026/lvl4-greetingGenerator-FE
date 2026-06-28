624573742624365b6ecff0a2f6bf1f53abde213c
Lubeth <lubethjimenez@yahoo.com>
2026-06-28 15:35:33 -0400

    Add npm install to React, added an input w/ a button onClick & a generateGreeting function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [name, setName] = useState("");
+
+  function generateGreeting() {
+    console.log(name)
+  }
 
   return (
     <>
       <h1>Welcome to our Project Page</h1>
+      <h2>Greeting Generator</h2>
+      <input type="text" placeholder="Enter your name" value={name} 
+      onChange={(event)=> setName(event.target.value)} />
+      <p>Your Name Is: {name}</p>
+      <button onClick={generateGreeting}>Generate Greeting</button>
     </>
   )
 }
